1e2ed4699ff370f41ca00147d8b6893f41cb23ad
Matthew Thorin <mthorin@byu.edu>
2024-10-03 16:05:02 -0600

    added tests from instruction

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,27 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from 'playwright-test-coverage';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('home page', async ({ page }) => {
+  await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  expect(await page.title()).toBe('JWT Pizza');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('buy pizza with login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Order now' }).click();
+  await expect(page.locator('h2')).toContainText('Awesome is a click away');
+  await page.getByRole('combobox').selectOption('1');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+  await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('d@jwt.com');
+  await page.getByPlaceholder('Email address').press('Tab');
+  await page.getByPlaceholder('Password').fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+  await expect(page.locator('tbody')).toContainText('Veggie');
+  await page.getByRole('button', { name: 'Pay now' }).click();
+  await expect(page.getByRole('main')).toContainText('0.008 ₿');
 });
