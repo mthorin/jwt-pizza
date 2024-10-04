d12bcfae3831ff38aabb22e8e17b44f3a7c92aa3
Matthew Thorin <mthorin@byu.edu>
2024-10-04 10:19:11 -0600

    fixing nyc coverage

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
+  build: { sourcemap: true },
   plugins: [
     istanbul({
       include: ['src/**/*'],
